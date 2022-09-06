7c75f0ba3b08ea7acd27f8e11125f4294c3db924
primitve3 <yun2193@hanmail.net>
2022-09-06 20:14:36 +0900

    yyyyy5

--- app/src/main/java/com/example/plogging/statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/plogging/statistics/StatisticsFragment.java
@@ -9,22 +9,47 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.plogging.R;
 
 public class StatisticsFragment extends Fragment {
+    ViewGroup rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_statistics, container, false);
+        rootView = (ViewGroup)inflater.inflate(R.layout.fragment_statistics, container, false);
 
         RecyclerView recyclerViewParent = rootView.findViewById(R.id.recyclerview_statistics);
         recyclerViewParent.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerViewParent.setAdapter(adapter);
-
-
+//        recyclerViewParent.setAdapter(adapter);
+        spinner();
 
         return rootView;
+    }
+    private void spinner(){
+        final String[] year = {"2022", "2021", "2020"};
+
+        Spinner spiner = rootView.findViewById(R.id.spinner1);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, year);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiner.setAdapter(adapter);
+
+        spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getContext(), "Selected Country: "+year[position] , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
     }
 }
