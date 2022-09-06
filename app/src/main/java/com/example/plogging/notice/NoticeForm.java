89b55c0096223e8f5d5bcf88f425ee8775fc34c5
just_user <marin6670@gmail.com>
2022-09-06 17:02:41 +0900

    NoticeForm에서 사진 첨부하는 코드 작성(테스트 필요)

--- app/src/main/java/com/example/plogging/notice/NoticeForm.java
+++ app/src/main/java/com/example/plogging/notice/NoticeForm.java
@@ -1,16 +1,40 @@
 package com.example.plogging.notice;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.plogging.R;
 
 public class NoticeForm extends AppCompatActivity {
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                Uri image_uri;
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if( result.getResultCode() == RESULT_OK && result.getData() != null){
+                        image_uri = result.getData().getData();
+                    }
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), image_uri);
 
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,4 +54,13 @@ public class NoticeForm extends AppCompatActivity {
 
         Toast.makeText(this,"Date: "+dateMessage, Toast.LENGTH_SHORT).show();
     }
+
+    public void getImageOnLocal(View view) {
+        //System.out.println("MainActivity.getImageOnLocal");
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityResult.launch(intent);
+    }
+
 }
