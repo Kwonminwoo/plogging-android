8472496d234af0fe3ec9d073d7f2a3649f44d55a
primitve3 <yun2193@hanmail.net>
2022-09-06 16:01:16 +0900

    yyyyy2

--- app/src/main/java/com/example/plogging/MainActivity.java
+++ app/src/main/java/com/example/plogging/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.plogging;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.plogging.home.HomeFragment;
 import com.example.plogging.notice.NoticeFragment;
@@ -53,5 +55,19 @@ public class MainActivity extends AppCompatActivity {
         homeFragment = new HomeFragment();
         noticeFragment = new NoticeFragment();
         statisticsFragment = new StatisticsFragment();
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day){
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this,"Date: "+dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/plogging/notice/NoticePost.java
+++ app/src/main/java/com/example/plogging/notice/NoticePost.java
@@ -2,19 +2,15 @@ package com.example.plogging.notice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.plogging.R;
 
 public class NoticePost extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_post);
-        Intent intent = getIntent();
-        int postId = intent.getIntExtra("postId", -1);
-        Toast.makeText(this, "" + postId, Toast.LENGTH_SHORT).show();
     }
 }
