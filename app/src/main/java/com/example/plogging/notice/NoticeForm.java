2b89979b349f94fca75cb1990327277cb83c3972
primitve3 <yun2193@hanmail.net>
2022-09-06 22:16:34 +0900

    yyyyy6

--- app/src/main/java/com/example/plogging/notice/NoticeForm.java
+++ app/src/main/java/com/example/plogging/notice/NoticeForm.java
@@ -4,18 +4,28 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.plogging.R;
+
+import org.w3c.dom.Text;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class NoticeForm extends AppCompatActivity {
     ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
@@ -47,20 +57,53 @@ public class NoticeForm extends AppCompatActivity {
     }
 
     public void processDatePickerResult(int year, int month, int day){
+        TextView date = (TextView) findViewById(R.id.date_text);
         String month_string = Integer.toString(month+1);
+        if (Integer.parseInt(month_string) < 10) {
+            month_string = "0" + month_string;
+        }
         String day_string = Integer.toString(day);
+        if (Integer.parseInt(day_string) < 10) {
+            day_string = "0" + day_string;
+        }
         String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        String dateMessage = (year_string + "-" + month_string + "-" + day_string);
 
+        date.setText(dateMessage);
         Toast.makeText(this,"Date: "+dateMessage, Toast.LENGTH_SHORT).show();
     }
 
     public void getImageOnLocal(View view) {
         //System.out.println("MainActivity.getImageOnLocal");
-
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
+
         startActivityResult.launch(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bm = getBitmapFromUri(uri) ;
+                image.setImageBitmap(bm);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
     }
 
 }
--- app/src/main/java/com/example/plogging/notice/NoticePost.java
+++ app/src/main/java/com/example/plogging/notice/NoticePost.java
@@ -2,7 +2,10 @@ package com.example.plogging.notice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.plogging.R;
 
@@ -12,5 +15,19 @@ public class NoticePost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_post);
+
+        Button btn = (Button) findViewById(R.id.notice_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("test", "test");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
+
+
 }
